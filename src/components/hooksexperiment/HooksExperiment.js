1292b268edf81cb3bb6dad5dbc7986a401b7c744
Bhabin <poon.bhabin6332@gmail.com>
2024-09-30 10:44:49 +0545

    hooks experiment component structure

---
+++ src/components/hooksexperiment/HooksExperiment.js
@@ -0,0 +1,16 @@
+function HooksExpepriment() {
+  return (
+    <>
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-primary" type="button">
+          Button
+        </button>
+        <button class="btn btn-primary" type="button">
+          Button
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default HooksExpepriment;
