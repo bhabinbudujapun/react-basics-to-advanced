edc8ba8a3fdcd23276525e53786eb98d6d51bf7c
Bhabin <poon.bhabin6332@gmail.com>
2024-09-30 14:00:28 +0545

    add hooks experiment compnent

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDom from "react-dom";
-import App from "./App";
+import ReactDom from "react-dom/client";
+// import App from "./App";
+import HooksExpepriment from "./components/hooksexperiment/HooksExperiment";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<HooksExpepriment />);
