9653278ecaa35de0991811898e90cec45b513339
Bhabin <poon.bhabin6332@gmail.com>
2024-10-01 09:36:54 +0545

    pass props/user object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import HooksExpepriment from "./components/hooksexperiment/HooksExperiment";
+import PropsExperiment from "./components/propsexperiment/PropsExperiment";
 
 function App() {
+  const user = {
+    uname: "Rehan Roy",
+    ubio: "I started my business with a passion for solving a problem. Through hard work and dedication, I turned my idea into a thriving company that provides value to our customers.",
+    ulink: "click here",
+  };
+
   return (
     <>
       <HooksExpepriment />
+      <PropsExperiment userinfo={user} />
     </>
   );
 }
