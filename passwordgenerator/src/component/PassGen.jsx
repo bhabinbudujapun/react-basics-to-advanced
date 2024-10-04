8f6cd7e19cd7f8c5b90a06d8116720970ea0dd2c
Bhabin <poon.bhabin6332@gmail.com>
2024-10-04 15:01:18 +0545

    update passgen component

--- passwordgenerator/src/component/PassGen.jsx
+++ passwordgenerator/src/component/PassGen.jsx
@@ -1,4 +1,14 @@
-export default function PassGen() {
+function PassGen({
+  password,
+  copyPassword,
+  length,
+  setLength,
+  numberAllow,
+  setNumberAllow,
+  charAllow,
+  setCharAllow,
+  passwordRef,
+}) {
   return (
     <>
       <div className="flex items-center">
@@ -6,27 +16,59 @@ export default function PassGen() {
           <h1 className="text-white text-center text-black font-semibold text-lg my-3">
             Password Generator
           </h1>
+
+          {/* Password Display */}
           <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input
               type="text"
               className="outline-none w-full py-1 px-3"
-              placeholder="Password"
+              placeholder="PASSWORD"
+              readOnly
+              value={password}
+              ref={passwordRef}
             />
-            <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            <button
+              className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+              onClick={copyPassword}>
               copy
             </button>
           </div>
+
+          {/* Password Options */}
           <div className="flex text-sm gap-x-2">
             <div className="flex items-center gap-x-1">
-              <input type="range" className="cursor-pointer" />
-              <label>Length: </label>
+              <input
+                type="range"
+                min="10"
+                max="20"
+                value={length}
+                onChange={(e) => {
+                  setLength(e.target.value);
+                }}
+                className="cursor-pointer"
+              />
+              <label>Length: {length} </label>
             </div>
             <div className="flex items-center gap-x-1">
-              <input type="checkbox" id="numberInput" />
+              <input
+                type="checkbox"
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllow((prev) => !prev);
+                }}
+                defaultChecked={numberAllow}
+              />
               <label htmlFor="numberInput">Numbers</label>
             </div>
             <div className="flex items-center gap-x-1">
-              <input type="checkbox" id="characterInput" />
+              <input
+                type="checkbox"
+                id="characterInput"
+                onChange={() => {
+                  setCharAllow((prev) => !prev);
+                }}
+                defaultChecked={charAllow}
+              />
               <label htmlFor="characterInput">Characters</label>
             </div>
           </div>
@@ -35,3 +77,5 @@ export default function PassGen() {
     </>
   );
 }
+
+export default PassGen;
