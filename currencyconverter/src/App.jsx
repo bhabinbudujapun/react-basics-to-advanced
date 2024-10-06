a588391b623f6fc9a0adfe48f7bfca898b277af5
Bhabin <poon.bhabin6332@gmail.com>
2024-10-06 09:02:01 +0545

    currency converter initial ui

--- currencyconverter/src/App.jsx
+++ currencyconverter/src/App.jsx
@@ -1,8 +1,42 @@
 function App() {
+  const BackgroundImage =
+    "https://static.vecteezy.com/system/resources/previews/002/151/430/large_2x/global-currency-exchange-icon-transfer-money-stock-market-abstract-background-vector.jpg";
+
   return (
-    <>
-      <h1 className="text-blue-500">Hello, react js</h1>
-    </>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('${BackgroundImage}')`,
+      }}>
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}>
+            <div className="w-full mb-1">
+              <label /> From
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5">
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <label />
+              To
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+              Convert
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
