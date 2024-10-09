397beaf7db9d08e4ae33f11358beaf4769cec2ed
Bhabin <poon.bhabin6332@gmail.com>
2024-10-09 09:25:10 +0545

    add layout, its include all the layout

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Layout from "./Layout";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1 className="p-4 bg-sky-400 text-center font-bold text-white">React Router</h1>
+    <Layout />
   </StrictMode>
 );
