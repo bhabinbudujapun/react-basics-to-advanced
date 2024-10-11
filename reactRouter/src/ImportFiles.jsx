20eaca2131a3268ee13019dde874323a392aaeed
Bhabin <poon.bhabin6332@gmail.com>
2024-10-11 08:06:50 +0545

    all the require file

---
+++ reactRouter/src/ImportFiles.jsx
@@ -0,0 +1,8 @@
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Header from "./components/Header/Header.jsx";
+
+export { Layout, Header, Home, About, Contact, Footer };
