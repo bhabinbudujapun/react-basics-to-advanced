2d3ea0879f32dc4cc266d9b506a756dd5fe8f7e5
Bhabin <poon.bhabin6332@gmail.com>
2024-10-11 08:08:12 +0545

    layout and route the file

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -1,7 +1,15 @@
-import Header from "./components/Header/Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "./ImportFiles";
 
 function Layout() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Layout, Home, Contact, About } from "./ImportFiles";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
