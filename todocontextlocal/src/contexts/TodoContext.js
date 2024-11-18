98ef47f176de653a7135343294b92d8dbf2d061b
Bhabin <poon.bhabin6332@gmail.com>
2024-11-18 08:52:02 +0545

    comment todo format

--- todocontextlocal/src/contexts/TodoContext.js
+++ todocontextlocal/src/contexts/TodoContext.js
@@ -1,13 +1,15 @@
 import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
-  todos: [
-    {
-      id: 1,
-      todo: "First Todo",
-      completed: false,
-    },
-  ],
+  // Demo/Example/Format
+  // todos: [
+  //   {
+  //     id: 1,
+  //     todo: "First Todo",
+  //     completed: false,
+  //   },
+  // ],
+
   addTodo: (todo) => {},
   updateTodo: (id, todo) => {},
   deleteTodo: (id) => {},
