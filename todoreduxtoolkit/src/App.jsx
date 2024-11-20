7fe5b99d900d826cdf87258c7c95ec58c1e29a52
Bhabin <poon.bhabin6332@gmail.com>
2024-11-20 08:07:04 +0545

    make/fix front part

--- todoreduxtoolkit/src/App.jsx
+++ todoreduxtoolkit/src/App.jsx
@@ -4,9 +4,15 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <>
-      <h1>Learn about ReduxToolKit</h1>
-      <AddTodo />
-      <Todo />
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl text-white font-bold text-center mb-8 mt-2">
+            Learn about ReduxToolKit
+          </h1>
+          <AddTodo />
+          <Todo />
+        </div>
+      </div>
     </>
   );
 }
