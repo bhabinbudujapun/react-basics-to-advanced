d774ed5d25c682fb3eee320e797a8f7305fc3eec
Bhabin <poon.bhabin6332@gmail.com>
2024-11-20 11:01:44 +0545

    add Todo-App name in project name

--- todoreduxtoolkit/src/App.jsx
+++ todoreduxtoolkit/src/App.jsx
@@ -7,8 +7,7 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl text-white font-bold text-center mb-8 mt-2">
-            Learn about ReduxToolKit
-            <h2>Todo-App</h2>
+            Learn about ReduxToolKit <br /> Todo-App
           </h1>
           <AddTodo />
           <Todo />
