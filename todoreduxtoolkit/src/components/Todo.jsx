c751c66e8425e9e6327fa827dc6f3e5d095626a4
Bhabin <poon.bhabin6332@gmail.com>
2024-11-22 11:42:49 +0545

    update/edit logic and render component

--- todoreduxtoolkit/src/components/Todo.jsx
+++ todoreduxtoolkit/src/components/Todo.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlice";
+import { updateTodo, removeTodo } from "../features/todo/todoSlice";
 
 function Todos() {
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
+
+  const [newText, setNewText] = useState("");
+  const [editingId, setEditingId] = useState(null);
+
+  const handleUpdateClick = (id, text) => {
+    setEditingId(id);
+    setNewText(text);
+  };
+
+  const handleSaveClick = (id) => {
+    dispatch(
+      updateTodo({
+        id,
+        text: newText,
+      })
+    );
+    setEditingId(null);
+  };
 
   return (
     <>
@@ -13,17 +31,42 @@ function Todos() {
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
             key={todo.id}>
-            <div className="text-white">{todo.text}</div>
-            <div>
-              <button
-                onClick={() => dispatch(removeTodo(todo.id))}
-                className="text-white bg-red-500 border-0 m-1 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
-                <i className="fa-solid fa-trash"></i>
-              </button>
-              <button className="text-white bg-red-500 border-0 m-1 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
-                <i className="fa-regular fa-pen-to-square"></i>
-              </button>
-            </div>
+            {editingId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={newText}
+                  onChange={(e) => setNewText(e.target.value)}
+                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <button
+                  type="submit"
+                  onClick={() => handleSaveClick(todo.id)}
+                  className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 rounded text-md">
+                  Save
+                </button>
+              </>
+            ) : (
+              <>
+                <div className="text-white">{todo.text}</div>
+                <div>
+                  <button
+                    onClick={() =>
+                      handleUpdateClick(
+                        dispatch(updateTodo({ id: todo.id, text: todo.text }))
+                      )
+                    }
+                    className="text-white bg-red-500 border-0 m-1 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
+                    <i className="fa-regular fa-pen-to-square"></i>
+                  </button>
+                  <button
+                    onClick={() => dispatch(removeTodo(todo.id))}
+                    className="text-white bg-red-500 border-0 m-1 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </div>
+              </>
+            )}
           </li>
         ))}
       </ul>
--- todoreduxtoolkit/src/features/todo/todoSlice.js
+++ todoreduxtoolkit/src/features/todo/todoSlice.js
@@ -20,9 +20,17 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id != action.payload);
     },
+
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      console.log(id, text);
+      state.todos = state.todos.map((todo) =>
+        todo.id === id ? { ...todo, text } : todo
+      );
+    },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, updateTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
