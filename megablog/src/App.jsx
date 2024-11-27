e70dd58af8821343cac8df00a7bd573f54eb7a03
Bhabin <poon.bhabin6332@gmail.com>
2024-11-27 08:18:25 +0545

    check tailwindcss installation

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID);
-  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID);
+  // console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
   return (
     <>
-      <h1>A blog app with appwrite</h1>
+      <div className="bg-gray-500">
+        <h1 className="text-white text-2xl py-3">A blog app with appwrite</h1>
+      </div>
     </>
   );
 }
