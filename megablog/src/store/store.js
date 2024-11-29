6ac2bcb2265f5131da15406d85e3de239801b10a
Bhabin <poon.bhabin6332@gmail.com>
2024-11-29 10:06:23 +0545

    add reducer name

--- megablog/src/store/store.js
+++ megablog/src/store/store.js
@@ -1,7 +1,8 @@
 import { configureState } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 
 const store = configureState({
-  reducer: {},
+  reducer: { authSlice },
 });
 
 export default store;
