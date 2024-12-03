aab45b929eec620d62f8487a9b0360a9d1435989
Bhabin <poon.bhabin6332@gmail.com>
2024-12-03 13:02:23 +0545

    import Button, Input component

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -1,5 +1,7 @@
 import container from "./Container/Container";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Button from "./Button";
+import Input from "./Input";
 
-export { container, Header, Footer };
+export { container, Header, Button, Input, Footer };
