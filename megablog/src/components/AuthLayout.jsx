5fb599e592c80c909e543def342016fdad9dc38e
Bhabin <poon.bhabin6332@gmail.com>
2024-12-05 09:05:54 +0545

    complete authlayout component

--- megablog/src/components/AuthLayout.jsx
+++ megablog/src/components/AuthLayout.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
-function AuthLayout() {
-  return <div>AuthLayout</div>;
+function Protected({ children, authentication = true }) {
+  const [loader, setLoader] = useState(true);
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authStatus && authStatus !== authentication) {
+      useNavigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <h1>loding...</h1> : <>children</>;
 }
 
-export default AuthLayout;
+export default Protected;
