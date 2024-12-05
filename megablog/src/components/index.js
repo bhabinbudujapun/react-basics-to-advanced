79d95c8f9f3d5d045d06179913b9c43d8e6bf6c0
Bhabin <poon.bhabin6332@gmail.com>
2024-12-05 14:00:35 +0545

    import RTE component

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -3,5 +3,6 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Button from "./Button";
 import Input from "./Input";
+import RTE from "./RTE";
 
-export { container, Header, Button, Input, Footer };
+export { container, Header, Button, Input, RTE, Footer };
