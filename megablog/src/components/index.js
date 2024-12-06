6095b00c28df5c6de45c905dd582654ddd8ff1f9
Bhabin <poon.bhabin6332@gmail.com>
2024-12-06 10:07:13 +0545

    importing necessary files

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -1,8 +1,23 @@
 import container from "./Container/Container";
+import AuthLayout from "./AuthLayout";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import PostCard from "./PostCard";
+import Signup from "./Signup";
 import Button from "./Button";
+import Login from "./Login";
 import Input from "./Input";
 import RTE from "./RTE";
 
-export { container, Header, Button, Input, RTE, Footer };
+export {
+  container,
+  Header,
+  Button,
+  Input,
+  RTE,
+  PostCard,
+  Login,
+  Signup,
+  AuthLayout,
+  Footer,
+};
