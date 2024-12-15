8ae1ecd09960c1345424db0adca20625bda60a35
Bhabin <poon.bhabin6332@gmail.com>
2024-12-15 23:16:52 +0545

    some ui/ux or frontend part changes

--- megablog/src/components/Signup.jsx
+++ megablog/src/components/Signup.jsx
@@ -8,8 +8,8 @@ import authService from "../appwrite/auth";
 
 function Signup() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const [error, setError] = useState("");
+  const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
 
   const create = async (data) => {
@@ -25,31 +25,34 @@ function Signup() {
       setError(error.message);
     }
   };
+
   return (
-    <div className="flex items-center justify-center w-full">
-      <div className="mx-auto w-full max-w-lg bg-gray-100 rounded p-10 border border-black/10">
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
         <div className="mb-2 flex justify-center">
-          <span className="inline-block w-full max-w[100px]">
+          <span className="inline-block w-full max-w-[100px]">
             <Logo width="100%" />
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in your account
+          Sign up to create account
         </h2>
-        <p className="mt-2 text-center text-base text-bold/60">
-          Don&apos;t have any account?&nbsp;{" "}
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline">
-            Sign Up
+            Sign In
           </Link>
         </p>
-        {error && <p className="text-center text-red-600 mt-8">{error}</p>}
-        <form onSubmit={handleSubmit(create)} className="mt-8">
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
             <Input
               label="Full Name: "
-              placeholder="Enter you name"
+              placeholder="Enter your full name"
               {...register("name", {
                 required: true,
               })}
@@ -61,14 +64,14 @@ function Signup() {
               {...register("email", {
                 required: true,
                 validate: {
-                  matchPattern: (value) =>
+                  matchPatern: (value) =>
                     /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
                     "Email address must be a valid address",
                 },
               })}
             />
             <Input
-              label="password: "
+              label="Password: "
               type="password"
               placeholder="Enter your password"
               {...register("password", {
