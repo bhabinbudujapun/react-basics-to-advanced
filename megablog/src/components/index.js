82603fec438538d4fe0118efd9bc22dd4cf81b68
Bhabin <poon.bhabin6332@gmail.com>
2024-12-20 22:20:56 +0545

    importing others required components for app

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -1,31 +1,30 @@
-import Container from "../components/Container/Container";
-import AuthLayout from "./AuthLayout";
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import PostCard from "./PostCard";
-import Signup from "./Signup";
-import Button from "./Button";
-import Login from "./Login";
-import Input from "./Input";
-import RTE from "./RTE";
+import Container from "./container/Container";
 import Logo from "./Logo";
-import Select from "./Select";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
 import PostForm from "./post-form/PostForm";
-
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
-  Container,
   Header,
-  Logo,
-  Select,
-  LogoutBtn,
-  Button,
-  Input,
-  RTE,
-  PostCard,
-  PostForm,
-  Login,
-  Signup,
-  AuthLayout,
   Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
 };
