1e168aa3f756e0a0a7c9244ee9bcf19518e83eb3
Bhabin <poon.bhabin6332@gmail.com>
2024-12-24 14:26:39 +0545

    correction on parameter calling and fix some typo

--- megablog/src/components/Select.jsx
+++ megablog/src/components/Select.jsx
@@ -1,17 +1,17 @@
-import React, { forwardRef, useId } from "react";
+import React, { useId } from "react";
 
-function Select({ options, className, label, ...props }, ref) {
-  const id = useId;
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
   return (
-    <div className="w-full ">
-      {label && <label htmlFor="id" className=""></label>}
+    <div className="w-full">
+      {label && <label htmlFor={id} className=""></label>}
       <select
         {...props}
-        id="id"
+        id={id}
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
         {options?.map((option) => (
-          <option value="{option}" key={option}>
+          <option key={option} value={option}>
             {option}
           </option>
         ))}
@@ -20,4 +20,4 @@ function Select({ options, className, label, ...props }, ref) {
   );
 }
 
-export default forwardRef(Select);
+export default React.forwardRef(Select);
